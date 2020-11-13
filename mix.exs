bf83dde172d7fb717fee159058e193716fd12515
Dominic Letz <dominic@diode.io>
2020-11-13 13:09:08 +0100

    Added license + description

--- mix.exs
+++ mix.exs
@@ -7,7 +7,14 @@ defmodule MixRebar3.MixProject do
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      # Hex
+      description: "mix_rebar3 is a rebar3 compiler for your mix project",
+      package: [
+        licenses: ["Apache 2.0"],
+        maintainers: ["Dominic Letz"],
+        links: %{"GitHub" => "https://github.com/dominicletz/mix_rebar3"}
+      ],
     ]
   end
 
